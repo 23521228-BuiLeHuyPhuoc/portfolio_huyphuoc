3674cdd7d27a140b739c00263fafff0b807807cc
Phuoc <23521228@gm.uit.edu.vn>
2026-07-22 10:42:14 +0700

    init commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-
+import { Geist, Geist_Mono, Dancing_Script} from "next/font/google";
+import "./globals.scss";
+import "./tailwind.css";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -10,6 +10,11 @@ const geistSans = Geist({
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
+});
+const dancingScript = Dancing_Script({
+  variable: "--font-dancing-script",
+  subsets: ["latin","vietnamese"],
+  weight: "400",
 });
 
 export const metadata: Metadata = {
@@ -25,7 +30,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${geistSans.variable} ${geistMono.variable} ${dancingScript.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
